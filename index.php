3e305bb981d33a0c2765d3c2a771b8ea45e1385f
Papp Gréta <papp.greta2@gmail.com>
2023-11-21 18:44:34 +0100

    Made index.php to make form

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+</head>
+
+<body>
+    <form action="process.php" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" name="username">
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" name="password">
+        <br>
+        <input type="submit" value="Login">
+
+    </form>
+</body>
+
+</html>
