28a5f0dc7487e02ffe72d9ed1ecdca15ba1b393e
Papp Gréta <papp.greta2@gmail.com>
2023-11-22 22:59:07 +0100

    Made the index.php better

--- index.php
+++ index.php
@@ -5,19 +5,79 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
-    <link rel="stylesheet" href="style.css">
+    <style>
+        body {
+            background-image: url(background.jpg);
+            min-height: 90vh;
+            background-size: cover;
+            background-position: center;
+            margin: 0;
+            display: flex;
+            align-items: center;
+
+        }
+
+        .container {
+            display: flex;
+            align-items: column;
+            justify-content: center;
+            padding-left: 10%;
+        }
+
+        form {
+            background-color: black;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+            color: white;
+            width: 400px;
+            height: 400px;
+            border: 2px solid orange;
+
+        }
+
+        label {
+            color: orange;
+            margin-bottom: 8px;
+            display: block;
+        }
+
+        input {
+            width: 100%;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: orange;
+            color: black;
+            cursor: pointer;
+            margin-top: 10%;
+        }
+
+        form label,
+        form input {
+            display: block;
+            margin: 0 auto;
+            text-align: center;
+            justify-content: center;
+            margin-top: 5%;
+            width: 80%;
+            height: 10%;
+        }
+    </style>
 </head>
 
 <body>
-    <form action="process.php" method="POST">
-        <label for="username">Username:</label>
-        <input type="text" name="username">
-        <br>
-        <label for="password">Password:</label>
-        <input type="password" name="password">
-        <br>
-        <input type="submit" value="Login">
-
+    <div class="container">
+        <form action="process.php" method="POST">
+            <label for="username">Username:</label>
+            <input type="text" name="username">
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" name="password">
+            <br>
+            <input type="submit" value="Login">
+    </div>
     </form>
 </body>
 
